f25cee2366437fae1f158e399ca1e4303460721e
Alice Santos <alicesantossilva345@gmail.com>
2022-11-17 19:02:27 -0300

    exercício 2

--- ex001/script.js
+++ ex001/script.js
@@ -8,15 +8,17 @@ function carregar(){
     if(hora >= 0 && hora < 12){
         img.src = 'imgs/manha.jpg'
         document.body.style.background = '#D0A61C'
-        //msg.innerHTML +=  'Bom Dia!'
+        msg.innerHTML +=  'Bom Dia!'
     }
     else if(hora >=12 && hora < 18){
         img.src = 'imgs/tarde.jpg'
         document.body.style.background = '#C0884F'
+        msg.innerHTML +=  'Boa Tarde!'
     }
     else {
         img.src = 'imgs/noite.jpg'
         document.body.style.background = '#835E89'
+        msg.innerHTML +=  'Boa Noite!'
     }
 }
 
--- ex002/script.js
+++ ex002/script.js
@@ -1,1 +1,24 @@
-alert('olá')
+function verificar(){
+    let data = new Date()
+    let ano = data.getFullYear()
+    let fano = document.getElementById('txtano')
+    let res = document.getElementById('res')
+    let img = document.getElementById('foto')
+    if(Number(fano.value) == 0 || Number(fano.value) > ano){
+        alert('verifique os dados e tente novamente.')
+    }
+    else{
+        let fsex = document.getElementsByName('radsex')
+        let idade = ano - Number(fano.value) //*
+        let genero = ''
+        if(fsex[0].checked){ //se o check está no 0 então é masculino
+            genero = 'masculino'
+            
+        }
+        else if(fsex[1].checked){
+            genero = 'feminino'
+        }
+        res.innerHTML = `<p>Detectamos uma pessoa do gênero <strong>${genero}</strong> com <strong>${idade}</strong> anos de idade.</p>`
+
+    }
+}
